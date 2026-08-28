d730cec8daeaf09f0e5c7ab90cc30a01ebb9e5c4
jhorgely15 <jhorgetrabajos@gmail.com>
2026-08-28 14:47:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,5 +14,9 @@ def reservas():
 def nueva_reserva():
     return "<h2>Crear nueva reserva</h2>"
 
+@app.route("/reservas/<int:id>/editar")
+def editar_reserva(id):
+    return f"<h2>Editando reserva {id}</h2>"
+
 if __name__ == "__main__":
     app.run(debug=True)
